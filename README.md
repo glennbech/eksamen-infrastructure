3e689e204ef340c44740159b396449720fd177f4
guberArmin <gubarm18@student.kristiania.no>
2020-11-18 00:35:36 +0100

    Added alert policy

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 [![Build Status](https://travis-ci.com/guberArmin/eksamen-infrastructure.svg?token=m6BpjWymm3UWnZ6QxDwC&branch=main)](https://travis-ci.com/guberArmin/eksamen-infrastructure)
+
+- hente key med travis encrypt og 
+- https://docs.opsgenie.com/docs/opsgenie-quick-start-guide
--- opsgenie.tf
+++ opsgenie.tf
@@ -5,7 +5,7 @@ provider "opsgenie" {
   #default is api.opsgenie.com
 }
 
-#https://registry.terraform.io/providers/opsgenie/opsgenie/latest/docs/resources/user
+#How to use: https://registry.terraform.io/providers/opsgenie/opsgenie/latest/docs/resources/user
 #Lets make some users
 resource "opsgenie_user" "first_test_user" {
   username = "first_test_user@gmail.com"
@@ -21,8 +21,8 @@ resource "opsgenie_user" "second_test_user" {
   timezone  = "Europe/Oslo"
 }
 
-#https://registry.terraform.io/providers/opsgenie/opsgenie/latest/docs/resources/team
-#Adde previously made users to team with members of users
+#How to use: https://registry.terraform.io/providers/opsgenie/opsgenie/latest/docs/resources/team
+#Make team of members. Member are users that are made in previouse stage
 resource "opsgenie_team" "eksam_team" {
   name        = "Eksam team"
   description = "This team is going to handle card api"
@@ -35,5 +35,51 @@ resource "opsgenie_team" "eksam_team" {
   member {
     id   = opsgenie_user.second_test_user.id
     role = "user"
+  }
+}
+
+#How to use:
+resource "opsgenie_alert_policy" "card_alerts" {
+  name               = "card policy"
+  team_id            = opsgenie_team.eksam_team.id
+  policy_description = "This policy is adjusting alerting for card api"
+  message            = "{{message}}"
+
+  filter {}
+  time_restriction {
+    type = "weekday-and-time-of-day"
+    # From 6 a clock on monday
+    # to 23 a clock on friday we are getting alerts
+    # Here I set up this API as "important"
+    # Since from monday to friday we can get alerts in middle of night
+    # Alternative was type = "time-of-day" and choose fixed time points for each day
+    # This depends on application
+    # Some have tu run all time, while other can be down for some time
+    restrictions {
+      end_day    = "friday"
+      end_hour   = 23
+      end_min    = 0
+      start_day  = "monday"
+      start_hour = 6
+      start_min  = 0
+    }
+    # Get alerts from 8 a clock to 21 a clock on saturday and sunday
+    # As it is weekend we get some time to rest
+    restrictions {
+      end_day    = "saturday"
+      end_hour   = 21
+      end_min    = 0
+      start_day  = "saturday"
+      start_hour = 8
+      start_min  = 0
+    }
+    restrictions {
+      end_day    = "sunday"
+      end_hour   = 21
+      end_min    = 0
+      start_day  = "sunday"
+      start_hour = 8
+      start_min  = 0
+    }
   }
 }
