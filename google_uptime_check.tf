e990484169baceeb1980651f4c060a6507062013
guberArmin <gubarm18@student.kristiania.no>
2020-11-19 13:12:25 +0100

    Testing google monitoring uptime check configuration

---
+++ google_uptime_check.tf
@@ -0,0 +1,22 @@
+resource "google_monitoring_uptime_check_config" "uptime-check" {
+  display_name = "http-uptime-check"
+  timeout      = "60s"
+
+  http_check {
+    path = "/api"
+    port = "8080"
+    request_method = "GET"
+  }
+
+  monitored_resource {
+    type = "uptime_url"
+    labels = {
+      project_id = var.project_id
+      host       = google_cloud_run_service.cards.status[0].url
+    }
+  }
+
+  content_matchers {
+    content = "Wellcome to homepage"
+  }
+}
