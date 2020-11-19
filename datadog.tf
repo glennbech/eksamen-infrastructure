678af25cec4b8d55083c277ea02e3970eb8b6ba2
guberArmin <gubarm18@student.kristiania.no>
2020-11-19 12:00:32 +0100

    Removed integration

--- datadog.tf
+++ datadog.tf
@@ -5,11 +5,4 @@ provider "datadog" {
 }
 
 
-# Create a new Datadog - Google Cloud Platform integration
-resource "datadog_integration_gcp" "awesome_gcp_project_integration" {
-  project_id     = local.gcp_json_data.project_id
-  private_key_id = local.gcp_json_data.private_key_id
-  private_key    = local.gcp_json_data.private_key
-  client_email   = local.gcp_json_data.client_email
-  client_id      = local.gcp_json_data.client_id
-}
+#
