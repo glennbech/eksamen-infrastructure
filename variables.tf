86af9d1b6c40fd4d29019b174cb11366376d1e63
guberArmin <gubarm18@student.kristiania.no>
2020-11-19 12:34:28 +0100

    Removed datadog

--- variables.tf
+++ variables.tf
@@ -7,14 +7,6 @@ variable "statuscake_key" {
 
 variable "opsgenie_key" {}
 
-#Datadog
-variable "api_key" {}
-variable "app_key" {}
-
 variable "project_id" {
   default = "devops-examen-2020"
-}
-
-locals {
-  gcp_json_data = jsondecode(file("${path.module}/google-key.json"))
 }
